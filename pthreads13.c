0a2f5ea65bc6feaa42e4a4c574a243ab1e893f78
Mark Fajet <markfajet@gmail.com>
2017-02-21 02:04:53 -0500

    system call done.

--- pthreads13.c
+++ pthreads13.c
@@ -1,5 +1,6 @@
 #include <linux/module.h>
 #include <linux/sched.h>
+#include <linux/string.h>
 struct task_struct *task, *t;
 
 
@@ -7,9 +8,24 @@ static int hello_init(void)
 {
     for_each_process(task){
 	       t = task;
-           do {
-               printk("Name: %s PID: [%d], parentPID: [%d] state: %ld\n", t->comm, t->pid,t->parent->pid, t->state);
-           }while_each_thread(task, t);
+           if(current->pid == t->pid){
+               printk("Printing all current process threads");
+               do {
+                   long l = t->state;
+                   char s[20];
+                   if(l==-1){
+                       strcpy(s, "UNRUNNABLE");
+                   }else if (l==0){
+                       strcpy(s, "RUNNING");
+                   }else{
+                       strcpy(s, "STOPPED");
+                   }
+                   printk("\tName: %s\t PID: [%d]\t parentPID: [%d]\t state: %s\n", t->comm, t->pid,t->parent->pid, s);
+               }while_each_thread(task, t);
+           }else {
+               printk("Name: %s\t PID: [%d]\t parentPID: [%d]\t state: %s\n", t->comm, t->pid,t->parent->pid, s);
+           }
+
      }
        return 0;
 }
---
+++ testprint.h
@@ -0,0 +1,3 @@
+#include<linux/unistd.h>
+#define __NR_fajet_mizrahi_paragas 341
+extern long int syscall (long int __sysno, ...) __THROW;
